d102d3b8562921c2151eef589ed0ca5d2a60585b
Dylan Yokoyama <amayokoy2@gmail.com>
2017-08-01 11:27:04 -0700

    leap

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,15 @@
-def is_leap_year():
-    pass
+def is_leap_year(year):
+
+    # First iteration
+    # if year % 4 == 0:
+    #     if year % 100 == 0:
+    #         if year % 400 == 0:
+    #             return True
+    #         else:
+    #             return False
+    #     else:
+    #         return True
+    # else:
+    #     return False
+
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)

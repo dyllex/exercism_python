7f2c751b5f5df04799007105f8e666818550f7a3
Dylan Yokoyama <amayokoy2@gmail.com>
2017-08-03 18:39:46 -0700

    refactoring

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,23 @@
-def to_rna():
-    pass
+"""
+I had a lot of trouble with this exercise.
+Would really appreciate any advice/tips. 
+Thanks!
+"""
+
+def to_rna(dna):
+
+    rna = []
+    dna_to_rna = {
+        'G': 'C',
+        'C': 'G',
+        'T': 'A',
+        'A': 'U'
+    }
+
+    for base in dna:
+        if base in 'GCAT':
+            rna += dna_to_rna[base]
+        else:
+            return ''
+
+    return ''.join(rna)
